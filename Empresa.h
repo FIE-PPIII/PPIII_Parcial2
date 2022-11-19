db178272838e3220d4d32b5a85b75d4dc2ec9e3e
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-11-19 12:51:22 -0300

    MOD - Empresa.h

--- Empresa.h
+++ Empresa.h
@@ -5,9 +5,27 @@
 #ifndef BASE_CPP_EMPRESA_H
 #define BASE_CPP_EMPRESA_H
 
+#include <iostream>
+#include "Zona.h"
+
+using namespace std;
 
 class Empresa {
+private:
+    map<int, Vendedor> vendedores;
+    map<int, Zona> zonas;
 
+public:
+    Empresa() {}
+    ~Empresa() {}
+
+    void agregarVendedor(int, string);
+    void agregarZona(int, string);
+    void vender(int, int, int, double);
+
+    void totalZonas();
+    void premiarVendedor();
+    void zonaMenorRecaudacionVendedores();
 };
 
 
