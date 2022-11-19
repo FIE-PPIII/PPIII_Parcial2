dd3a618ce6beaee8944c1eff093b53be6c3852d6
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-11-19 13:08:31 -0300

    Update Zona.h

--- Zona.h
+++ Zona.h
@@ -5,10 +5,33 @@
 #ifndef BASE_CPP_ZONA_H
 #define BASE_CPP_ZONA_H
 
+#include "Venta.h"
+#include <map>
+
+using namespace std;
 
 class Zona {
+private:
+    int idZona;
+    string nombre;
+    map<int, Venta> ventas;
 
+public:
+    Zona() {}  // para map
+    Zona(int, string);
+    ~Zona();
+
+    int getId();
+    string getNombre();
 };
+
+inline int Zona::getId() {
+    return this->idZona;
+}
+
+inline string Zona::getNombre() {
+    return this->nombre;
+}
 
 
 #endif //BASE_CPP_ZONA_H
