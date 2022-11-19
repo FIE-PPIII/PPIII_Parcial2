946bc57f0ef7cf75e7e68d542c1b04b0e654d5d9
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-11-19 12:57:58 -0300

    MOD - Vendedor.h

--- Vendedor.h
+++ Vendedor.h
@@ -5,10 +5,44 @@
 #ifndef BASE_CPP_VENDEDOR_H
 #define BASE_CPP_VENDEDOR_H
 
+#include <string>
+
+using namespace std;
 
 class Vendedor {
+private:
+    int idVendedor;
+    string nombre;
+    double recaudacion;
+
+public:
+    Vendedor(int, string);
+    Vendedor() {}  // Para usar operador [] en map
+
+    ~Vendedor();
+
+    int getId();
+    string getNombre();
+
+    double getRecaudacion();
+    void setRecaudacion(double);
 
 };
 
+inline int Vendedor::getId() {
+    return this->idVendedor;
+}
+
+inline string Vendedor::getNombre() {
+    return this->nombre;
+}
+
+inline double Vendedor::getRecaudacion() {
+    return this->recaudacion;
+}
+
+inline void Vendedor::setRecaudacion(double val) {
+    this->recaudacion = val;
+}
 
 #endif //BASE_CPP_VENDEDOR_H
