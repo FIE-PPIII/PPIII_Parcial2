af39a33f313047036202ca0c8b64f98e7c5b578a
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-11-19 13:03:16 -0300

    Update Venta.h

--- Venta.h
+++ Venta.h
@@ -5,10 +5,36 @@
 #ifndef BASE_CPP_VENTA_H
 #define BASE_CPP_VENTA_H
 
+#include "Vendedor.h"
+
+using namespace std;
 
 class Venta {
+private:
+    int idVenta;
+    double monto;
+    Vendedor* vendedor;
 
+public:
+    Venta(int, double, Vendedor*);
+    Venta() {}  // para map
+    ~Venta() {}
+
+    int getId();
+    double getMonto();
+    Vendedor* getVendedor();
 };
 
+inline int Venta::getId() {
+    return this->idVenta;
+}
+
+inline double Venta::getMonto() {
+    return this->monto;
+}
+
+inline Vendedor* Venta::getVendedor() {
+    return this->vendedor;
+}
 
 #endif //BASE_CPP_VENTA_H
